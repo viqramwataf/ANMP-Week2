aa3b8efc4838944fbab4cd816979173ee23834e0
viqramwataf <s160420119@Student.ubaya.ac.id>
2023-03-08 14:23:16 +0700

    init commit

---
+++ app/src/main/java/com/ubaya/adv160420119week2/GameFragment.kt
@@ -0,0 +1,29 @@
+package com.ubaya.adv160420119week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_game.*
+
+class GameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnBack = view.findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            val action = GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
---
+++ app/src/main/java/com/ubaya/adv160420119week2/MainFragment.kt
@@ -0,0 +1,29 @@
+package com.ubaya.adv160420119week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnStart = view.findViewById<Button>(R.id.btnStart)
+        btnStart.setOnClickListener {
+            val action = MainFragmentDirections.actionGameFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
